4c3ef7365a1b94ec6606acb30f464bd215d21229
Mohammad Khalil <moegts@gmail.com>
2021-09-05 23:54:36 +0300

    solve the lap

---
+++ src/Components/HornedBeasts.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class HornedBeasts extends Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image_url} alt='imgReact' title={this.props.title}/>
+                <p>{this.props.description}</p>
+                <h4>{this.props.keyword}</h4>
+                <h4>{this.props.horns}</h4>
+            </div>
+        )
+    }
+}
+
+export default HornedBeasts
