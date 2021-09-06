18d98c4e45625d91c9aa26c45293192cc25adb40
Mohammad Khalil <moegts@gmail.com>
2021-09-06 20:57:56 +0300

    lap02

--- src/Components/HornedBeasts.js
+++ src/Components/HornedBeasts.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 class HornedBeasts extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            LikesCounter:0,
+        }
+    }
+    plus1Like=()=>{
+        this.setState({
+            LikesCounter:this.state.LikesCounter+1
+        })
+    }
     render() {
         return (
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt='imgReact' title={this.props.title}/>
+                <img onClick={this.plus1Like} src={this.props.image_url} alt='imgReact' title={this.props.title}/>
+                <h3>👍🏼{this.state.LikesCounter}🧡</h3>
                 <p>{this.props.description}</p>
                 <h4>{this.props.keyword}</h4>
                 <h4>{this.props.horns}</h4>
